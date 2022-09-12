213ebba4a018cb3dd97a818de27e80f3b5184d51
JakeSchlei <jakeschlei@gmail.com>
2022-09-12 09:13:22 -0700

    questions are working and data is appearing, fixed error calling for string

--- index.js
+++ index.js
@@ -3,105 +3,115 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const promptUser = readMeData => {
+  return inquirer.prompt([
     {
-        type: 'input',
-        name: 'projectName',
-        message: 'What is the title of your project? (Required)',
-        validate: projectNameInput => {
-            if (projectNameInput) {
-                return true;
-            }else {
-                console.log('Please enter a project name!');
-                return false;
-            }
+      type: "input",
+      name: "projectName",
+      message: "What is the title of your project? (Required)",
+      validate: (projectNameInput) => {
+        if (projectNameInput) {
+          return true;
+        } else {
+          console.log("Please enter a project name!");
+          return false;
         }
+      },
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Provide some information about the project'
+      type: "input",
+      name: "description",
+      message: "Provide some information about the project",
     },
     {
-        type: 'confirm',
-        name: 'installInstructions',
-        message: 'Does this project require any installation instructions?',
-        default: true
+      type: "confirm",
+      name: "installInstructions",
+      message: "Does this project require any installation instructions?",
+      default: true,
     },
     {
-        type: 'input',
-        name: 'install',
-        message: 'Please provide the install instructions for the project',
-        when: ({installInstructions}) => {
-            if (installInstructions) {
-                return true;
-            } else {
-                return false;
-            }
+      type: "input",
+      name: "install",
+      message: "Please provide the install instructions for the project",
+      when: ({ installInstructions }) => {
+        if (installInstructions) {
+          return true;
+        } else {
+          return false;
         }
+      },
     },
     {
-        type: 'input',
-        name: 'usageInfo',
-        message: 'Please describe the usage information about this project'
+      type: "input",
+      name: "usageInfo",
+      message: "Please describe the usage information about this project",
     },
     {
-        type: 'input',
-        name: 'guidelines',
-        message: 'Please provide the contribution guidelines here'
+      type: "input",
+      name: "guidelines",
+      message: "Please provide the contribution guidelines here",
     },
     {
-        type: 'input',
-        name: 'testInst',
-        message: 'Please provide any test instructions here'
+      type: "input",
+      name: "testInst",
+      message: "Please provide any test instructions here",
     },
     {
-        type: 'checkbox',
-        name: 'license',
-        message: 'What license will be associated with this project?',
-        choices: [
-            'The Unlicense',
-            'Boost Software License',
-            'MIT License',
-            'Apache License',
-            'Mozilla Public License',
-            'GNU AGP'
-        ]
+      type: "checkbox",
+      name: "license",
+      message: "What license will be associated with this project?",
+      choices: [
+        "The Unlicense",
+        "Boost Software License",
+        "MIT License",
+        "Apache License",
+        "Mozilla Public License",
+        "GNU AGP",
+      ],
     },
     {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            }else {
-                console.log('Please enter your Username!');
-                return false;
-            }
-          }
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your Username!");
+          return false;
+        }
+      },
     },
     {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            }else {
-                console.log('Please enter your email!');
-                return false;
-            }
-          }
+      type: "input",
+      name: "email",
+      message: "Enter your email address (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your email!");
+          return false;
+        }
+      },
     },
+  ]);
+};
 
-];
+promptUser()
+.then(readMeData => {
+    console.log(readMeData);
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// fs.writeFile('./README.md', questions, err => {
+//     if (err) throw err;
+    
+//     console.log('Read me complete! Check the file to see the results!');
+// });
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
